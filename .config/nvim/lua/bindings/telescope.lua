f839059eee422a55d1c41060fdffb63f14776423
Sourajyoti Basak <wiz28@protonmail.com>
2021-10-02 14:22:28 +0530

    feat(nvim): Add `git_branches` keybinding

--- .config/nvim/lua/bindings/telescope.lua
+++ .config/nvim/lua/bindings/telescope.lua
@@ -4,7 +4,7 @@ wk.register({
 	['<leader>fg'] = { '<cmd>Telescope git_files<cr>', 'Find git file' },
 	['<leader>fr'] = { '<cmd>Telescope live_grep<cr>', 'Grep every line in current directory' },
 	['<leader>fb'] = { '<cmd>Telescope current_buffer_fuzzy_find<cr>', 'Grep every line in current buffer' },
-	['<leader>fh'] = { '<cmd>Telescope oldfiles<cr>', 'Find recent files' },
+	['<leader>fh'] = { '<cmd>Telescope git_branches<cr>', 'Find git branches' },
 	['<leader>fc'] = { '<cmd>Telescope colorscheme<cr>', 'Find color scheme' },
 	['<leader>fm'] = { '<cmd>Telescope marks<cr>', 'Find marks' },
 	['<leader>fl'] = { '<cmd>Telescope lsp_workspace_diagnostics<cr>', 'Find LSP diagnostics' },
