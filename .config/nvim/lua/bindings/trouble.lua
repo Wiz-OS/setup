fbba94e0260da6d7abf664be2e93843a96de6a2c
Sourajyoti Basak <wiz28@protonmail.com>
2021-10-03 22:48:55 +0530

    feat(nvim): Add trouble keybindings

---
+++ .config/nvim/lua/bindings/trouble.lua
@@ -0,0 +1,18 @@
+-- =============================================================================
+--  __      __________   ______
+-- /  \    /  \_____  \ /  __  \
+-- \   \/\/   //  ____/ >      <
+--  \        //       \/   --   \
+--   \__/\  / \_______ \______  /
+--        \/          \/      \/
+-- trouble.lua --- keybindings for touble
+-- Copyright (c) 2021 Sourajyoti Basak
+-- Author: Sourajyoti Basak < wiz28@protonmail.com >
+-- URL: https://github.com/wizard-28/dotfiles/
+-- License: MIT
+-- =============================================================================
+
+wk.register({
+	['<leader>t'] = { name = "Trouble" },
+	['<leader>tt'] = { '<cmd>TroubleToggle<cr>', "Toggle trouble" },
+})
