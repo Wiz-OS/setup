c840ac0e2c401ccf2df3b70d849df014a1c762dd
Sourajyoti Basak <wiz28@protonmail.com>
2021-10-03 16:42:06 +0530

    feat(nvim): Add `trouble`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -220,6 +220,17 @@ return require('packer').startup(function(use)
 		end
 	}
 	use {
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
+	}
+	use {
 		'a-vrma/black-nvim',
 		ft = 'python',
 		config = function()
