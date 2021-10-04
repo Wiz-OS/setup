c954e5e2c03abba94faa30e560f167e619e2f080
Sourajyoti Basak <wiz28@protonmail.com>
2021-10-04 15:36:02 +0530

    feat(nvim): Lazy load `trouble`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -229,12 +229,10 @@ return require('packer').startup(function(use)
 	use {
 		"folke/trouble.nvim",
 		requires = "kyazdani42/nvim-web-devicons",
+		cmd = 'TroubleToggle',
+		module = 'trouble',
 		config = function()
-			require("trouble").setup {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			}
+			require("trouble").setup()
 		end
 	}
 	use {
