3d6bd4e61fcb42fc69bb36fec2eeb25cc89645f8
Sourajyoti Basak <wiz28@protonmail.com>
2022-03-21 14:29:58 +0530

    feat(fish): add `gprune` function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,63 +13,80 @@
 # =============================================================================
 
 if status --is-interactive
-# =============================================================================
-# Startup commands
-# =============================================================================
-starship init fish | source &
-treefetch --bonsai &
-zoxide init fish | source &
-# =============================================================================
+    # =============================================================================
+    # Startup commands
+    # =============================================================================
+    starship init fish | source &
+    treefetch --bonsai &
+    zoxide init fish | source &
+    # =============================================================================
 
-fish_vi_cursor
-export PATH="/home/pop-os/.local/bin:$PATH"
+    fish_vi_cursor
+    export PATH="/home/pop-os/.local/bin:$PATH"
 
-# =============================================================================
-# Functions
-# =============================================================================
-# git sync
-function gsync
-	git reset --hard HEAD~1
-	git push -f
-	git pull upstream $argv --rebase
-	git push
-end
+    # =============================================================================
+    # Functions
+    # =============================================================================
+    # git sync
+    function gsync
+        git reset --hard HEAD~1
+        git push -f
+        git pull upstream $argv --rebase
+        git push
+    end
 
-# Jump around in code bases
-function dev
-	set work_folders $HOME/dotfiles/ /media/pop-os/SBASAK/code
-	set file (fdfind --hidden --exclude ".git" --type f . $work_folders | fzf --preview "bat --color=always --style=numbers --line-range=:500 {}") && \
-	cd (dirname "$file") && \
-	nvim "$file" && \
-	commandline -f repaint
-end
+    function gprune
+        set force false
+        switch $argv
+            case -f
+                set force true
+        end
+        for branch in (git for-each-ref --format='%(refname:short)' refs/heads) do
+            # find configured upstream, if any
+            set remote (git config --get branch.$branch.remote) || continue
+            # # if tracking local branch, skip
+            if [ "$remote" = . ]
+                continue
+            end
+            # if the upstream commit resolves, skip
+            set ucommit (git rev-parse "$branch@{u}" 2> /dev/null) && continue
+            # upstream is invalid - remove local branch, or print removal
+            $force && git branch -D $branch || echo "git branch -D $branch"
+        end
+    end
 
-# qrcode
-function qrcode
-          set input "$argv"
-          [ -z "$input" ] && local input="@/dev/stdin"
-          curl -d "$input" https://qrcode.show
-end
-# =============================================================================
 
-# =============================================================================
-# Aliases
-# =============================================================================
-alias l="exa --icons -F"
-alias la="exa --icons -aF"
-alias ll="exa --icons -Flah"
-alias bcat="batcat"
-# =============================================================================
+    # Jump around in code bases
+    function dev
+        set work_folders $HOME/dotfiles/ /media/pop-os/SBASAK/code
+        set file (fdfind --hidden --exclude ".git" --type f . $work_folders | fzf --preview "bat --color=always --style=numbers --line-range=:500 {}") && cd (dirname "$file") && nvim "$file" && commandline -f repaint
+    end
 
-# =============================================================================
-# Key bindings
-# =============================================================================
-function fish_user_key_bindings
-	bind --mode insert \co 'dev'
-end
-# =============================================================================
+    # qrcode
+    function qrcode
+        set input "$argv"
+        [ -z "$input" ] && local input="@/dev/stdin"
+        curl -d "$input" https://qrcode.show
+    end
+    # =============================================================================
+
+    # =============================================================================
+    # Aliases
+    # =============================================================================
+    alias l="exa --icons -F"
+    alias la="exa --icons -aF"
+    alias ll="exa --icons -Flah"
+    alias bcat="batcat"
+    # =============================================================================
+
+    # =============================================================================
+    # Key bindings
+    # =============================================================================
+    function fish_user_key_bindings
+        bind --mode insert \co dev
+    end
+    # =============================================================================
 end
 
 # Generated for envman. Do not edit.
 test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
-
