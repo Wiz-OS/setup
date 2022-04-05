f3826987774b84ea54221d97ce5a280c3b8dde32
Sourajyoti Basak <wiz28@protonmail.com>
2022-04-06 02:21:15 +0530

    temp

--- .config/waybar/modules/waybar-wttr.py
+++ .config/waybar/modules/waybar-wttr.py
@@ -89,7 +89,11 @@ def format_chances(hour: Dict[str, str]) -> str:
     return ", ".join(conditions)
 
 
-data["text"] = weather["current_condition"][0]["temp_C"]
+data["text"] = (
+    weather["current_condition"][0]["temp_C"]
+    + "° "
+    + WEATHER_CODES[weather["current_condition"][0]["weatherCode"]]
+)
 
 data[
     "tooltip"
